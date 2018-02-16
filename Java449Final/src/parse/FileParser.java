ef6248e55a714584930d44141b18442178e86eb2
Dan <demisangdan@gmail.com>
2018-02-15 20:23:37 -0700

    Update FileParser.java

--- Java449Final/src/parse/FileParser.java
+++ Java449Final/src/parse/FileParser.java
@@ -271,17 +271,12 @@ public class FileParser {
 						// convert the line to an array in order to access
 						// individual penalty value
 						lineArray = line.toCharArray();
-<<<<<<< HEAD
 						
 						try {
 							String temp = Character.toString(lineArray[0]);
 							Integer.parseInt(temp);
 						}
 						catch (Exception e) {
-=======
-
-						if (!(Character.getNumericValue(lineArray[0]) > 9 | Character.getNumericValue(lineArray[0]) < 0)) {
->>>>>>> branch 'master' of https://github.com/nickjimenez/449JavaFinal.git
 							int[] forcedList = {};
 							OutputWriter.writeFile(forcedList, 0, "Error while parsing input file");
 							System.exit(0);
